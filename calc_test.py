bf7d5e8eff06342227d4e35e89c2bfbdfac3f950
tagalsan <santagal19@gmail.com>
2026-04-25 14:21:52 +0530

    quality check3

--- calc_test.py
+++ calc_test.py
@@ -4,7 +4,7 @@ import pytest
 from calc import add, substract, multiply, division
 
 def test_add():
-    assert add(6, 2) == 8
+    assert add(6, 2) == 5
 
 def test_sub():
     assert substract(6, 2) == 4
@@ -13,4 +13,4 @@ def test_mul():
     assert multiply(2, 3) == 6
 
 def test_div():
-    assert division(12, 3) == 4
+    assert division(12, 3) == 1
