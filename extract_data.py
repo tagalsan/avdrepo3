b7a9379485060de40aca2f98e4cea477bf541fe7
tagalsan <santagal19@gmail.com>
2026-04-25 11:59:33 +0530

    install dependancy2

--- extract_data.py
+++ extract_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 data = {
      "id":[1, 2, 3],
-     "name": ["A", "B", "C"]
-     "age":[10, 20, 30]   
+     "name": ["A", "B", "C"],
+     "age": [10, 20, 30]   
 }
 
 df = pd.dataframe(data)
